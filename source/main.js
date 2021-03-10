be0326b21480ba08c93a570413f98b0f6e98fd2f
11notes <github@11notes.ch>
2021-03-10 14:22:28 +0100

    hello world

--- source/main.js
+++ source/main.js
@@ -2,11 +2,11 @@
 const http = require(`http`);
 
 const requestHandler = function(req, res){
-    res.end(`Hello from main.js!`);
+    res.end(`hello world!`);
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(8080, function(err){
-    console.log(`Server main.js started!`);
+    console.log(`HTTP server started at port 8080`);
 });
